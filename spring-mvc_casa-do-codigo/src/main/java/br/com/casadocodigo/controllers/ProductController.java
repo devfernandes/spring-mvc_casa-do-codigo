138700b708e32ad12cd1aeefb2351e92216eb824
devrafacarvalho <devrafacarvalho@gmail.com>
2020-04-26 18:39:43 -0300

    TransactionManager added!

--- spring-mvc_casa-do-codigo/src/main/java/br/com/casadocodigo/controllers/ProductController.java
+++ spring-mvc_casa-do-codigo/src/main/java/br/com/casadocodigo/controllers/ProductController.java
@@ -1,5 +1,7 @@
 package br.com.casadocodigo.controllers;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,7 @@ import br.com.casadocodigo.daos.ProductDAO;
 import br.com.casadocodigo.models.Product;
 
 @Controller
+@Transactional
 public class ProductController {
 	
 	@Autowired
@@ -16,7 +19,7 @@ public class ProductController {
 	@RequestMapping("/produtos")
 	public String save(Product product) {
 		productDAO.save(product);
-		System.out.println("Cadastrando o produto");
+		System.out.println("Cadastrando o produto: " + product);
 		return "products/ok";
 	}
 
